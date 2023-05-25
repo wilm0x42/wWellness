a97f1de895d7b442edecea858ec67d4fd50f7167
wilm0x42 <wilm0x42@startmail.com>
2023-05-25 16:01:20 -0400

    Frick your autoformatting

--- sleepwebhook.py
+++ sleepwebhook.py
@@ -7,16 +7,17 @@ import requests
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        print(
-            "Usage: sleepwebhook.py <@someone's discord ID> https://discord.com/your/webhook/url"
-        )
+        print("Usage: sleepwebhook.py <@someone's discord ID> https://discord.com/your/webhook/url")
         exit(1)
-    ping = sys.argv[
-        1]  # Use format "<@336685325231325184>" to have it actually ping
+    
+    ping = sys.argv[1] # Use format "<@336685325231325184>" to have it actually ping
     discord_url = sys.argv[2]
+    
     sleep_lines = []
+    
     with open("sleep_lines.txt") as f:
         sleep_lines = f.readlines()
+    
     for line in sleep_lines:
         line.replace("{ping}", ping)
 
@@ -32,8 +33,7 @@ if __name__ == "__main__":
             "content": ping,
         }
         result = requests.post(discord_url, json=data)
-        data[
-            "content"] = "https://cdn.discordapp.com/attachments/430078268037660686/899758205901635604/unknown.png"
+        data["content"] = "https://cdn.discordapp.com/attachments/430078268037660686/899758205901635604/unknown.png"
 
     result = requests.post(discord_url, json=data)
 
